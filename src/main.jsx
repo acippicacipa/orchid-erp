894d64abf268854db5f16ebc2cb63ca7f090f7d3
acippicacipa <marketing.studio16@gmail.com>
2025-12-26 10:12:11 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ToastProvider } from './hooks/use-toast-provider'; // Impor provider Anda
+import { Toaster } from './components/ui/toaster'; // Asumsi Anda punya komponen Toaster
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+      <Toaster /> {/* Letakkan Toaster di sini agar bisa mengakses context */}
+    </ToastProvider>
   </StrictMode>,
 )
